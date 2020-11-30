cb156d8680ae82e891113b75e668543d7bc625b7
Suraj Bhatta <60405010+Suraj13078@users.noreply.github.com>
2020-11-30 11:55:18 -0600

    Update query.js

--- database/query.js
+++ database/query.js
@@ -1,1 +1,46 @@
 
+const mysql = require("mysql");
+const mysqlConfig = require("./config.js");
+
+const connection = mysql.createConnection(mysqlConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.log("Error connecting to the database");
+  } else {
+    console.log("Connected to the database");
+  }
+});
+
+// select links from logIn, adminLinks where logIn.adminRole = adminLinks.adminRole;
+
+const getRole = (userName, callback) => {
+  connection.query(
+    "SELECT links FROM adminLinks WHERE adminRole=?",
+    [userName],
+    (err, data) => {
+      if (err) {
+        console.log("problem getting all links in query");
+        callback(err, null);
+      } else {
+        callback(null, data);
+      }
+    }
+  );
+};
+
+const registerUsers = (userName, adminRole, password, callback) => {
+  connection.query(
+    `INSERT INTO logIn (userName, adminRole, password) VALUES ('${userName}','${adminRole}','${password}')`,
+    (err, data) => {
+      if (err) {
+        console.log("problem posting reviews in query");
+        callback(err, null);
+      } else {
+        callback(null, data);
+      }
+    }
+  );
+};
+
+module.exports = { registerUsers, getRole };
